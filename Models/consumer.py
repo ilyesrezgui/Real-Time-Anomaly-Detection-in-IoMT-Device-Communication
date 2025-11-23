8ca54f8b1baaf0a841244aedd37b37e3fa00122b
inessmahjoub <inessmahjoob@gmail.com>
2025-11-23 19:53:57 +0100

    save the model

---
+++ Models/consumer.py
@@ -0,0 +1,35 @@
+import os
+import json
+import joblib
+import numpy as np
+from kafka import KafkaConsumer
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+import tensorflow as tf
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+print("RUNNING FROM:", os.getcwd())
+
+# -------------------------
+# Load LSTM model, scaler, threshold
+# -------------------------
+model = tf.keras.models.load_model(
+    r"C:\Users\MSI\Videos\Downloads\Real-Time-Anomaly-Detection-in-IoMT-Device-Communication-1\Models\lstm_autoencoder.h5",
+    custom_objects={"mse": tf.keras.losses.MeanSquaredError()}
+)
+
+scaler = joblib.load(
+    r"C:\Users\MSI\Videos\Downloads\Real-Time-Anomaly-Detection-in-IoMT-Device-Communication-1\Models\scaler.pkl"
+)
+
+with open(r"C:\Users\MSI\Videos\Downloads\Real-Time-Anomaly-Detection-in-IoMT-Device-Communication-1\Models\numeric_features.json", "r") as f:
+    numeric_features = json.load(f)
+
+with open(r"C:\Users\MSI\Videos\Downloads\Real-Time-Anomaly-Detection-in-IoMT-Device-Communication-1\Models\threshold.json", "r") as f:
+    threshold = json.load(f)["threshold"]
+
+timesteps = len(numeric_features)
+n_features = 1
+
